efdba48a75f9de3a56638823fb7978cfd7b919cf
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2024-03-12 15:48:59 +0530

    undo unnecessary change

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		url: 'http://localhost:4173'
+		port: 4173
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
